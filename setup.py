3f8af5fc32ed79f44546e75c35a659bd28d17c81
Supraja Ramesh <supraja.ramesh@fau.de>
2023-11-05 17:12:17 +0100

    Added requirements.txt, __init__.py, setup.py and updated readme

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_2',
+    version='1.0',
+    packages=['math_quiz'],
+    url='',
+    license='',
+    author='suprajaramesh',
+    author_email='supraja.ramesh@fau.de',
+    description=''
+)
