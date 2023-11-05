0d2970a9bf2f50a91d3ffa4944b6886734578452
Supraja Ramesh <supraja.ramesh@fau.de>
2023-11-05 16:47:41 +0100

    Code clean in math_quiz,py and added testcases

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,62 @@
 import random
 
-
-def function_A(min, max):
+def generate_random_number(min, max):
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def select_random_math_operator():
+    """
+    Random math operator selection.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def perform_math_operation(num1, num2, operator):
+    """
+    Perform given math operation on 2 numbers.
+    """
+    problem = f"{num1} {operator} {num2}"
+    if operator == '+': answer = num1 + num2
+    elif operator == '-': answer = num1 - num2
+    else: answer = num1 * num2
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    total_questions = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        # get all values for the problem
+        num1 = generate_random_number(1, 10); num2 = generate_random_number(1, 5)
+        operator = select_random_math_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        # get correct solution
+        problem, answer = perform_math_operation(num1, num2, operator)
+        print(f"\nQuestion: {problem}")
 
-        if useranswer == ANSWER:
+        # get user input
+        user_answer = input("Your answer: ")
+
+        try:
+            user_answer = int(user_answer)
+        except:
+            print(f"Invalid answer. The correct answer is {answer}.")
+            continue
+
+        # validate user answer
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_number, select_random_math_operator, perform_math_operation
 
 
-class TestMathGame(unittest.TestCase):
+class TestMathQuiz(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_number(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_select_random_math_operator(self):
+        # Test if random operator generated is within the chosen operators
+        operator = select_random_math_operator()
+        self.assertIn(operator, ['+', '-', '*'])
 
-    def test_function_C(self):
-            test_cases = [
+    def test_perform_math_operation(self):
+        # Test if problem and answer are generated in the expected way
+        test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (2, 3, '*', '2 * 3', 6),
+                (0, 3, '*', '0 * 3', 0),
+                (9, 7, '-', '9 - 7', 2)
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            actual_problem, actual_answer = perform_math_operation(num1, num2, operator)
+            self.assertEquals(expected_problem, actual_problem)
+            self.assertEquals(expected_answer, actual_answer)
 
 if __name__ == "__main__":
     unittest.main()
+
